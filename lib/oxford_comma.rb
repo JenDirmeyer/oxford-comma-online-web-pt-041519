ccd1e223079556a997b342d81ec5c932ae1c40ba
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-05-10 02:08:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,7 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   else
-    array.insert(-2, " and ")
     array.join(", ")
-    array.delete(-1, ",")
+    array.insert(-2, " and ")
   end  
 end
